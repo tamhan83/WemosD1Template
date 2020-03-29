44acb7a88937fd3079d2b241b32f7a075b3bfaaa
Hannes Tammerle <hannes.tammerle@gmail.com>
2020-03-29 23:30:38 +0100

    button

--- WemosButton/WemosButton.ino
+++ WemosButton/WemosButton.ino
@@ -1,6 +1,6 @@
 // constants won't change. They're used here to
 // set pin numbers:
-const int buttonPin = 2;     // the number of the pushbutton pin
+const int buttonPin = 0;     // the number of the pushbutton pin
 const int ledPin = 13;      // the number of the LED pin
 // variables will change:
 int buttonState = 0;         // variable for reading the pushbutton status
@@ -10,7 +10,7 @@ void setup() {
     // initialize the LED pin as an output:
     //pinMode(ledPin, OUTPUT);
     // initialize the pushbutton pin as an input:
-    //pinMode(buttonPin, INPUT_PULLUP);
+    //pinMode(buttonPin, INPUT_PULLDOWN_16);
     Serial.println("hi");
 }
 void loop() {
